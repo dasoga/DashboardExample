230cac6e43b2f0a8f5b4b722f132f4437e3fa8ea
Dante Solorio <ddasogaa@gmail.com>
2016-06-10 13:38:56 -0500

    Initial Commit

---
+++ DashboardDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DashboardDemo
+//
+//  Created by Dante Solorio on 6/10/16.
+//  Copyright © 2016 Dasoga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DashboardDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DashboardDemo
+//
+//  Created by Dante Solorio on 6/10/16.
+//  Copyright © 2016 Dasoga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
